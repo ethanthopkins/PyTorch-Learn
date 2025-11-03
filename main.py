af09df841aeb1104c99c7bb8ba735c019ca7e376
ethan <ethanthopkins@gmail.com>
2025-11-03 11:58:45 -0700

    makes prediction

--- main.py
+++ main.py
@@ -102,4 +102,26 @@ def createModel():
 #loads model
 model = NeuralNetwork().to(device)
 model.load_state_dict(torch.load("model.pth", weights_only=True))
-print("loaded!")
+print("Loaded!")
+
+#make predictions
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
